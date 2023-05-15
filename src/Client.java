f1ae7bc0f55878c1e1ced32680135294ee8e0cb1
ishan058 <ishankhadkaik246@gmail.com>
2023-05-15 10:47:47 +0545

    learning UDP

--- src/Client.java
+++ src/Client.java
@@ -1,3 +1,18 @@
-public class Client {
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 
+public class Client {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Client!");
+        DatagramSocket socket = new DatagramSocket(12345);
+        byte[] data = new byte[1024];
+        DatagramPacket packet = new DatagramPacket(data, data.length);
+
+        //receive the message
+        socket.receive(packet);
+        String message = new String(packet.getData(), 0, packet.getLength());
+        System.out.println(message);
+        socket.close();
+    }
 }
--- src/Server.java
+++ src/Server.java
@@ -1,11 +1,17 @@
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 
 public class Server {
     public static void main(String[] args) throws IOException {
         DatagramSocket socket = new DatagramSocket();
         String message = "Hello world";
-        DatagramPacket packet = new DatagramPacket();
+        InetAddress address = InetAddress.getByName("localhost"); //used to address
+        DatagramPacket packet = new DatagramPacket(message.getBytes(), message.length(), address, 12345);
+
+        //sending message
+        socket.send(packet);
+        socket.close();
     }
 }
